0c065dd3588e442bdd2ea848a1982b0595a545e3
MakerSquare Student <karapendl@gmail.com>
2013-06-19 14:47:09 -0500

    generating csv to store data

---
+++ hailinator.rb
@@ -0,0 +1,24 @@
+require "twitter"
+require "csv"
+
+
+Twitter.configure do |config|
+  config.consumer_key = "lUjBX3ocP4aPY4yHsDOQ"
+  config.consumer_secret = "HPbD4rUrX9JAaztYQivy7eDDTWxCjmWrJMfKpzWWI"
+  config.oauth_token = "260868608-T1OAcwQMfS6PynVCBy6WFRSiu48ZQgFI4XBUBTSJ"
+  config.oauth_token_secret = "QwP1cgQ1jpKsdfoDwo8MJvpE4GMlDvyiwO2TTriz4cU"
+end
+
+
+search = Twitter.search("hail")
+
+
+CSV.open("hailinator.csv", "wb") do |csv|
+	csv << ["handle", "text", "url"]
+	search.results.each do |result|
+		result.from_user
+		result.full_text
+		result.urls
+		csv << ["#{result.from_user}", "#{result.full_text}", "#{result.urls.to_s}"]
+	end
+end
